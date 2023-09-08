744505717a6d2a4fc6f3ab06dab4c0a817458cc3
Jszir <123523194+Jszir@users.noreply.github.com>
2023-09-08 09:07:18 -0500

    Changes package.json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Importación adicional de estilos
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.innerHTML = galleryItems
+  .map(({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__pic" href="${original}">
+            <img
+            class="gallery__image"
+            data-source="${original}"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </li>`
+  )
+  .join("");
+  
+    galleryContainer.style.marginTop =  '100px'
+
+
+galleryContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  const originalUrl = e.target.dataset.source;
+  basicLightbox.create(`<img src="${originalUrl}">`).show();
+});
+
+
